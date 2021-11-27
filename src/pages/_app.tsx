d5cadbf3c6fe2d2f75eb94a17a3000e94d7ab013
JHSeo <qnfqnfqnf@gmail.com>
2021-11-27 23:05:31 +0900

    Add next-themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
+import globalStyles from '@/styles/globalStyles'
+import { darkThemeClassName, lightThemeClassName } from '@stitches.js'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  globalStyles()
+
+  return (
+    <ThemeProvider
+      disableTransitionOnChange
+      attribute="class"
+      value={{ light: lightThemeClassName, dark: darkThemeClassName }}
+      defaultTheme="system"
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
