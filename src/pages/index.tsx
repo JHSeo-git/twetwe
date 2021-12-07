06b4c3a85e166d7193762117d9190f854f48d8a7
JHSeo <qnfqnfqnf@gmail.com>
2021-12-07 23:03:50 +0900

    Add textarea, input styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import Texlider from '@/components/animated/Texlider'
-import { borderFlashAnimation } from '@/styles/animation'
+import TextareaAutosize from 'react-textarea-autosize'
 import { styled } from '@stitches.js'
+import { useRef } from 'react'
 
 function Home() {
-  // TODO: styledInput component 빼야됨
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+  // TODO: style된 component 빼야됨
   return (
     <Container>
       <Texlider text="TWE" />
-      <StyledInput type="text" placeholder="퉤" />
+      <Box>
+        <StyledInput type="text" placeholder="Subject" />
+        <StyledTextArea rows={5} ref={textareaRef} placeholder="Short" />
+      </Box>
     </Container>
   )
 }
@@ -19,36 +24,77 @@ const Container = styled('section', {
   ai: 'center',
 })
 
+const Box = styled('div', {
+  maxWidth: '$maxWidth',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+
+  px: '$3',
+})
+
 const StyledInput = styled('input', {
   outline: 'none',
 
-  width: '$maxWidth',
-  mx: '$3',
+  width: '100%',
 
   py: '$3',
   px: '$4',
-  br: '$pill',
+  br: '$4',
   bc: 'inherit',
   transition:
     'box-shadow .2s ease, transform .5s ease, background-color .5s ease',
 
   fontFamily: 'inherit',
   fontSize: '$base',
-  color: '$mauve11',
-  textAlign: 'center',
+  color: '$mauve12',
 
   border: '1px solid transparent',
-  animation: `${borderFlashAnimation} 4s ease infinite`,
+
+  mb: '$4',
 
   '&::placeholder': {
-    color: 'transparent',
-    transition: 'color .2s ease',
+    // color: 'transparent',
+    transition: 'color .4s ease',
   },
 
   '&:focus, &:focus-visible': {
     bc: '$sand1',
     boxShadow: '$muiDp16',
-    // transform: 'translateY(-5px)',
+    animation: 'none',
+    '&::placeholder': {
+      color: 'transparent',
+    },
+  },
+})
+
+const StyledTextArea = styled(TextareaAutosize, {
+  outline: 'none',
+  resize: 'none',
+
+  width: '100%',
+
+  py: '$3',
+  px: '$4',
+  br: '$4',
+  bc: 'inherit',
+  transition:
+    'box-shadow .2s ease, transform .5s ease, background-color .5s ease',
+
+  fontFamily: 'inherit',
+  fontSize: '$base',
+  color: '$mauve12',
+
+  border: '1px solid transparent',
+
+  '&::placeholder': {
+    // color: 'transparent',
+    transition: 'color .4s ease',
+  },
+
+  '&:focus, &:focus-visible': {
+    bc: '$sand1',
+    boxShadow: '$muiDp16',
     animation: 'none',
     '&::placeholder': {
       color: 'transparent',
