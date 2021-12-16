22e5e31678b5d331bb8241f97d1288ea298345e8
JHSeo <qnfqnfqnf@gmail.com>
2021-12-16 22:14:31 +0900

    Add authState in `<AuthForm>`

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -1,9 +1,16 @@
 import { styled } from '@stitches.js'
+import { useState } from 'react'
 import BaseInput from '../common/BaseInput'
 
 export type AuthFormProps = {}
 
+type AuthState = 'login' | 'signup'
+
 function AuthForm(props: AuthFormProps) {
+  const [authState, setAuthState] = useState<AuthState>('login')
+  const onClickAuthState = () => {
+    setAuthState((prev) => (prev === 'login' ? 'signup' : 'login'))
+  }
   return (
     <Form>
       <FormInput
@@ -13,7 +20,17 @@ function AuthForm(props: AuthFormProps) {
         autoComplete="off"
       />
       <FormInput name="password" type="password" placeholder="Password" />
-      <SubmitButton type="submit">Submit</SubmitButton>
+      <SubmitButton type="submit">
+        {authState === 'login' ? 'Login' : 'Sign Up'}
+      </SubmitButton>
+      <Comment>
+        {authState === 'login'
+          ? 'Already have an account?'
+          : 'Don’t have an account?'}
+        <ButtonLikeLink onClick={onClickAuthState} type="button">
+          {authState === 'login' ? 'login' : 'sign up'}
+        </ButtonLikeLink>
+      </Comment>
     </Form>
   )
 }
@@ -42,6 +59,8 @@ const SubmitButton = styled('button', {
   p: '$2',
   br: '$1',
 
+  mb: '$2',
+
   transition: 'background-color .2s ease',
 
   '@hover': {
@@ -49,6 +68,19 @@ const SubmitButton = styled('button', {
       bc: '$blue3',
     },
   },
+})
+
+const Comment = styled('p', {
+  fontSize: '$sm',
+})
+
+const ButtonLikeLink = styled('button', {
+  cursor: 'pointer',
+  border: 'none',
+  bc: 'inherit',
+  color: '$blue9',
+
+  textTransform: 'capitalize',
 })
 
 export default AuthForm
