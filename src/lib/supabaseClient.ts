f4e66b7fa0048619f7ce932686dab098e929c580
JHSeo <qnfqnfqnf@gmail.com>
2021-12-17 22:59:10 +0900

    Refactor supabaseClient

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,4 +4,4 @@ import { SUPABASE_KEY, SUPABASE_URL } from './constants'
 if (!SUPABASE_URL) throw new Error('Missing SUPABASE_URL')
 if (!SUPABASE_KEY) throw new Error('Missing SUPABASE_KEY')
 
-export const client = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY)
