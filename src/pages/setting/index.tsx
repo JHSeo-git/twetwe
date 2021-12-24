d6b0c802d48687c970a4870a39e33752b9e753c3
JHSeo <qnfqnfqnf@gmail.com>
2021-12-24 19:42:11 +0900

    Add sample toggles

--- src/pages/setting/index.tsx
+++ src/pages/setting/index.tsx
@@ -2,22 +2,26 @@ import ToggleButton from '@/components/primitives/ToggleButton'
 import Panel from '@/components/common/Panel'
 import { styled } from '@stitches.js'
 
-export type SettingPageProps = {}
-
-function SettingPage(props: SettingPageProps) {
+function SettingPage() {
   return (
     <Container>
-      <Panel>
-        <ToggleButton label="토글" />
-        <ToggleButton />
-        <ToggleButton />
-      </Panel>
+      <SettingPanel>
+        <ToggleButton label="자동로그인" />
+        <ToggleButton label="설정할것들" />
+        <ToggleButton size="small" label="작은토글" />
+      </SettingPanel>
     </Container>
   )
 }
 
 const Container = styled('section', {
-  pt: '$4',
+  pt: '$8',
+})
+
+const SettingPanel = styled(Panel, {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$4',
 })
 
 export default SettingPage
